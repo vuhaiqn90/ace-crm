ff0ee7b1d63e4ac51ae147def8d95366b496c5f9
khangnguyen_16021990 <duykhang@pcb-graphtech.com.vn>
2019-07-26 13:13:26 +0700

    Fix

--- sale_loyalty/models/sale.py
+++ sale_loyalty/models/sale.py
@@ -64,7 +64,8 @@ class SaleOrder(models.Model):
                         points += (pp_currency_point + pp_product_point)
                     order.points_won = points
             if order.partner_id:
-                order.temp_points_won = order.points_won
+                self.env.cr.execute("""update sale_order set temp_points_won = %s where id = %s""" % (points, order.id))
+                # order.temp_points_won = order.points_won
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
