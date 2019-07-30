611ea0ac56b90ff764501aeb8ad940c395089b74
khangnguyen_16021990 <duykhang@pcb-graphtech.com.vn>
2019-07-30 14:48:19 +0700

    Fix

--- sale_loyalty/models/res_partner.py
+++ sale_loyalty/models/res_partner.py
@@ -6,12 +6,12 @@ class ResPartner(models.Model):
     loyalty_points = fields.Float(help='The loyalty points the user won as part of a Loyalty Program')
     membership_id = fields.Many2one('membership.level', string="Membership")
 
-    # @api.multi
-    # def write(self, vals):
-    #     res = super(ResPartner, self).write(vals)
-    #     for partner in self:
-    #         membership_id = self.env['membership.level'].search([('point', '<=', partner.loyalty_points)],
-    #                                                             order='point', limit=1)
-    #         if membership_id and (not partner.membership_id or partner.membership_id.point < partner.loyalty_points):
-    #             partner.membership_id = membership_id.id
-    #     return res
+    @api.multi
+    def write(self, vals):
+        res = super(ResPartner, self).write(vals)
+        for partner in self:
+            membership_id = self.env['membership.level'].search([('point', '<=', partner.loyalty_points)],
+                                                                order='point', limit=1)
+            if membership_id and (not partner.membership_id or partner.membership_id.point < partner.loyalty_points):
+                partner.membership_id = membership_id.id
+        return res
