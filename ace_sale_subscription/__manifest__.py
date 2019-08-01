2687c20eb7fbb2cb543a64744169fc7a3991117e
vuhaiqn90 <vuhai@aceconomy.net>
2019-08-01 14:01:26 +0700

    T0160 Sale Subscription

---
+++ ace_sale_subscription/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ACECONOMY SALE SUBSCRIPTION',
+    'category': 'ACEconomy',
+    'author': 'pcb-graphtech.com.vn',
+    'description': '',
+    'depends': [
+        'sale_subscription',
+    ],
+    'data': [
+        'data/cron.xml',
+        'views/sale_subscription_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
