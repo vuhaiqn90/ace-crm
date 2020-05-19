8aee8b7fab10714e7ce5692914184ad2cd047237
vuhaiqn90 <vuhai@aceconomy.net>
2020-05-19 13:33:52 +0700

    Acad

--- ace_acad/models/product.py
+++ ace_acad/models/product.py
@@ -5,5 +5,5 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    request_cost = fields.Boolean('Request for Cost')
-    request_url = fields.Char('URL')
+    request_quote = fields.Boolean('Request for quote')
+    request_url = fields.Char('Quote URL')
