5bf04339d0bf31c16659c0ac5cc3999abf533c49
vuhaiqn90 <vuhai@aceconomy.net>
2020-05-29 10:10:43 +0700

    Fix bug

--- ace_tmg/models/partner.py
+++ ace_tmg/models/partner.py
@@ -10,24 +10,36 @@ class ResPartner(models.Model):
     job_position = fields.Selection([('sale', 'Salesperson'), ('tels', 'Telesales'), ('ctv', 'CTV')],
                                     string='Job Position')
 
+    def _get_name(self):
+        """ Utility method to allow name_get to be overrided without re-browse the partner """
+        partner = self
+        name = partner.ref and '[%s] %s' % (partner.ref, partner.name) or partner.name
+
+        if partner.company_name or partner.parent_id:
+            if not name and partner.type in ['invoice', 'delivery', 'other']:
+                name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
+            if not partner.is_company:
+                name = self._get_contact_name(partner, name)
+        if self._context.get('show_address_only'):
+            name = partner._display_address(without_company=True)
+        if self._context.get('show_address'):
+            name = name + "\n" + partner._display_address(without_company=True)
+        name = name.replace('\n\n', '\n')
+        name = name.replace('\n\n', '\n')
+        if self._context.get('address_inline'):
+            name = name.replace('\n', ', ')
+        if self._context.get('show_email') and partner.email:
+            name = "%s <%s>" % (name, partner.email)
+        if self._context.get('html_format'):
+            name = name.replace('\n', '<br/>')
+        if self._context.get('show_vat') and partner.vat:
+            name = "%s ‒ %s" % (name, partner.vat)
+        return name
+
     @api.multi
     def name_get(self):
         result = []
         for partner in self:
-            name = partner.ref and '[%s] %s' % (partner.ref, partner.name) or partner.name
-            if self._context.get('show_address_only'):
-                name = partner.vietnam_full_address
-            if self._context.get('show_address'):
-                name = name + "\n" + partner.vietnam_full_address
-            name = name.replace('\n\n', '\n')
-            name = name.replace('\n\n', '\n')
-            if self._context.get('address_inline'):
-                name = name.replace('\n', ', ')
-            if self._context.get('show_email') and partner.email:
-                name = "%s <%s>" % (name, partner.email)
-            if self._context.get('html_format'):
-                name = name.replace('\n', '<br/>')
-            if self._context.get('show_vat') and partner.vat:
-                name = "%s ‒ %s" % (name, partner.vat)
+            name = partner._get_name()
             result.append((partner.id, name))
         return result
