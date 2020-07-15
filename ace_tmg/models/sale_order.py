6a08aecdfa2f7bab7a1e2b053a27fe6345f7de5e
vuhaiqn90 <vuhai@aceconomy.net>
2020-07-15 14:36:31 +0700

    T0673 Khoa chiet khau tren don hang

--- ace_tmg/models/sale_order.py
+++ ace_tmg/models/sale_order.py
@@ -17,32 +17,32 @@ class SaleOrder(models.Model):
         invoice_vals.update({'telesales': self.telesales and self.telesales.id or False})
         return invoice_vals
 
-    @api.multi
-    def check_limit(self):
-        self.ensure_one()
-        if self.user_has_groups('sales_team.group_sale_manager'):
-            return True
-        partner = self.partner_id
-        user_id = self.env['res.users'].search([
-            ('partner_id', '=', partner.id)], limit=1)
-        if user_id and not user_id.has_group('base.group_portal') or not \
-                user_id:
-            moveline_obj = self.env['account.move.line']
-            movelines = moveline_obj.search([
-                ('partner_id', '=', partner.id),
-                ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
-                ('move_id.state', '=', 'posted'),
-            ])
-            credit = sum(line.credit for line in movelines)
-            debit = sum(line.debit for line in movelines)
-            available_credit_limit = partner.credit_limit - debit + credit
-            if self.amount_total > available_credit_limit:
-                msg = 'Your available credit limit' \
-                      ' Amount = %s \nCheck "%s" Accounts or Credit ' \
-                      'Limits.' % (available_credit_limit,
-                                   self.partner_id.name)
-                raise UserError(_('You can not confirm Sale Order. \n' + msg))
-            return True
+    # @api.multi
+    # def check_limit(self):
+    #     self.ensure_one()
+    #     if self.user_has_groups('sales_team.group_sale_manager'):
+    #         return True
+    #     partner = self.partner_id
+    #     user_id = self.env['res.users'].search([
+    #         ('partner_id', '=', partner.id)], limit=1)
+    #     if user_id and not user_id.has_group('base.group_portal') or not \
+    #             user_id:
+    #         moveline_obj = self.env['account.move.line']
+    #         movelines = moveline_obj.search([
+    #             ('partner_id', '=', partner.id),
+    #             ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+    #             ('move_id.state', '=', 'posted'),
+    #         ])
+    #         credit = sum(line.credit for line in movelines)
+    #         debit = sum(line.debit for line in movelines)
+    #         available_credit_limit = partner.credit_limit - debit + credit
+    #         if self.amount_total > available_credit_limit:
+    #             msg = 'Your available credit limit' \
+    #                   ' Amount = %s \nCheck "%s" Accounts or Credit ' \
+    #                   'Limits.' % (available_credit_limit,
+    #                                self.partner_id.name)
+    #             raise UserError(_('You can not confirm Sale Order. \n' + msg))
+    #         return True
 
     @api.multi
     def action_cancel(self):
